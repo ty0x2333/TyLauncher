70832d012ed4bac0dc210b19e07c10813f2c422b
luckytianyiyan@gmail.com <luckytianyiyan@gmail.com>
2015-09-18 06:17:29 +0800

    M AppUtils.

--- utils/apputils.cpp
+++ utils/apputils.cpp
@@ -1,45 +1,41 @@
 #include "apputils.h"
 #include "TyLog_Qt.h"
+
 #ifdef Q_OS_WIN32
-#include <windows.h>
+    #include <windows.h>
 #elif (defined(Q_OS_LINUX) || defined(Q_OS_MAC))
-#include <sys/types.h>  
-#include <sys/stat.h>  
-#include <fcntl.h>  
-#include <unistd.h>  
+    #include <sys/types.h>
+    #include <sys/stat.h>
+    #include <fcntl.h>
+    #include <unistd.h>
 #endif
+
 bool AppUtils::isRunTimeOnly()
 {
 #ifdef Q_OS_WIN32
-    // 创建互斥量  
-    HANDLE m_hMutex = CreateMutex(NULL, FALSE,  L"TyyAppManager" );  
+    // 创建互斥量
+    HANDLE m_hMutex = CreateMutex(NULL, FALSE,  L"TyyAppManager" );
     // 检查错误代码
-    if(GetLastError() == ERROR_ALREADY_EXISTS)  {  
+    if(GetLastError() == ERROR_ALREADY_EXISTS){
         // 如果已有互斥量存在则释放句柄并复位互斥量
         CloseHandle(m_hMutex);
         m_hMutex  =  NULL;
         return false;
     }
-    else{
-        return true;
-    }
 #elif (defined(Q_OS_LINUX) || defined(Q_OS_MAC))
-    const char filename[]  = "/tmp/tyyappmanager_lockfile";  
+    const char filename[]  = "/tmp/tyyappmanager_lockfile";
     int fd = open(filename, O_WRONLY | O_CREAT , 0644);
     // 使用文件区域做进程锁
-    int flock = lockf(fd, F_TLOCK, 0 );  
+    int flock = lockf(fd, F_TLOCK, 0 );
     if (fd == -1) {
         TyLogCritical("open lockfile error/n");
-        return false;  
+        return false;
     }
     // 给文件加锁
     if (flock == -1) {
         TyLogCritical("lock file error/n");
         return false;
-    }  
-    // 程序退出后, 文件自动解锁
+    }
+#endif
     return true;
-#else
-    return true;
-#endif  
 }
