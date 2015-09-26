6c51cac3be62b4abf6c3d4d659ca797b6b17cc0b
luckytianyiyan@gmail.com <luckytianyiyan@gmail.com>
2015-09-26 20:32:21 +0800

    M language zh_CN

--- language/zh_CN.ts
+++ language/zh_CN.ts
@@ -4,300 +4,298 @@
 <context>
     <name>AboutDialog</name>
     <message>
-        <location filename="aboutdialog.ui" line="14"/>
+        <location filename="../aboutdialog.ui" line="14"/>
         <source>About</source>
         <translation>关于</translation>
     </message>
     <message>
-        <location filename="aboutdialog.ui" line="26"/>
-        <source>OK</source>
-        <translation>确定</translation>
+        <location filename="../aboutdialog.ui" line="53"/>
+        <source>AppName</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="aboutdialog.cpp" line="11"/>
-        <source>Name: </source>
-        <translation>名称:</translation>
+        <location filename="../aboutdialog.ui" line="60"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Developer: &lt;a href=&quot;mailto:luckytianyiyan@gmail.com&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;luckytianyiyan@gmail.com&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="aboutdialog.cpp" line="12"/>
+        <location filename="../aboutdialog.ui" line="72"/>
         <source>Version: </source>
-        <translation>版本号:</translation>
+        <translation>版本号: </translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog.ui" line="79"/>
+        <source>unknow</source>
+        <translation>未知</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog.ui" line="101"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;My Website: &lt;a href=&quot;http://www.tianyiyan.com&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;http://www.tianyiyan.com&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog.cpp" line="10"/>
+        <source>About </source>
+        <translation>关于 </translation>
     </message>
 </context>
 <context>
     <name>AppButtonDialog</name>
     <message>
-        <location filename="appbuttondialog.ui" line="14"/>
+        <location filename="../appbuttondialog.ui" line="14"/>
         <source>Edit Button Properties</source>
-        <translation>编辑按钮属性</translation>
+        <translation>编辑按钮配置</translation>
     </message>
     <message>
-        <location filename="appbuttondialog.ui" line="39"/>
-        <source>OK</source>
-        <translation>确定</translation>
-    </message>
-    <message>
-        <location filename="appbuttondialog.ui" line="58"/>
-        <source>Cancel</source>
-        <translation>取消</translation>
-    </message>
-    <message>
-        <location filename="appbuttondialog.ui" line="90"/>
+        <location filename="../appbuttondialog.ui" line="58"/>
         <source>Name :</source>
-        <translation>名称 :</translation>
+        <translation>名称: </translation>
     </message>
     <message>
-        <location filename="appbuttondialog.ui" line="125"/>
+        <location filename="../appbuttondialog.ui" line="80"/>
         <source>Target :</source>
-        <translation>目标 :</translation>
+        <translation>目标: </translation>
     </message>
     <message>
-        <location filename="appbuttondialog.ui" line="141"/>
-        <source>Open Folder</source>
-        <translation>打开文件夹</translation>
-    </message>
-    <message>
-        <location filename="appbuttondialog.ui" line="157"/>
+        <location filename="../appbuttondialog.ui" line="120"/>
         <source>Open File</source>
         <translation>打开文件</translation>
     </message>
     <message>
-        <location filename="appbuttondialog.cpp" line="42"/>
-        <source>Browse File</source>
-        <translation>选择文件</translation>
+        <location filename="../appbuttondialog.ui" line="130"/>
+        <source>Open Folder</source>
+        <translation>打开文件夹</translation>
     </message>
     <message>
-        <location filename="appbuttondialog.cpp" line="57"/>
+        <location filename="../appbuttondialog.cpp" line="33"/>
+        <source>Browse File</source>
+        <translation>文件浏览</translation>
+    </message>
+    <message>
+        <location filename="../appbuttondialog.cpp" line="48"/>
         <source>Browse Folder</source>
-        <translation>选择文件夹</translation>
+        <translation>文件夹浏览</translation>
+    </message>
+</context>
+<context>
+    <name>AppConfigDialog</name>
+    <message>
+        <location filename="../appconfigdialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../appconfigdialog.ui" line="24"/>
+        <source>&amp;General</source>
+        <translation>&amp;一般</translation>
+    </message>
+    <message>
+        <location filename="../appconfigdialog.ui" line="39"/>
+        <source>Language</source>
+        <translation>语言</translation>
     </message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="mainwindow.ui" line="17"/>
-        <source>TyyAppManager</source>
-        <translation></translation>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>TyLauncher</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="80"/>
+        <location filename="../mainwindow.ui" line="67"/>
+        <location filename="../mainwindow.ui" line="180"/>
         <source>Settings</source>
         <translation>设置</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="84"/>
+        <location filename="../mainwindow.ui" line="71"/>
         <source>Theme</source>
         <translation>主题</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="91"/>
-        <source>Language</source>
-        <translation>语言</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="102"/>
+        <location filename="../mainwindow.ui" line="81"/>
         <source>Help</source>
         <translation>帮助</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="109"/>
+        <location filename="../mainwindow.ui" line="88"/>
         <source>File</source>
         <translation>文件</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="118"/>
-        <source>View</source>
-        <translation>视图</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="129"/>
+        <location filename="../mainwindow.ui" line="103"/>
         <source>HotKey...</source>
-        <translation>热键...</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="132"/>
+        <location filename="../mainwindow.ui" line="106"/>
         <source>Set HotKey...</source>
-        <translation>设置热键...</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="140"/>
+        <location filename="../mainwindow.ui" line="114"/>
         <source>Hide</source>
-        <translation></translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="143"/>
+        <location filename="../mainwindow.ui" line="117"/>
         <source>Esc</source>
-        <translation></translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="148"/>
-        <location filename="mainwindow.ui" line="151"/>
-        <source>Show TyyAppManager</source>
-        <translation>显示主窗口</translation>
+        <location filename="../mainwindow.ui" line="122"/>
+        <location filename="../mainwindow.ui" line="125"/>
+        <source>Show TyLauncher</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="156"/>
+        <location filename="../mainwindow.ui" line="130"/>
         <source>About</source>
         <translation>关于</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="167"/>
+        <location filename="../mainwindow.ui" line="141"/>
         <source>Default Theme</source>
         <translation>默认主题</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="175"/>
+        <location filename="../mainwindow.ui" line="149"/>
         <source>System Theme</source>
         <translation>系统主题</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="186"/>
-        <source>Always On Top</source>
-        <translation>窗口置顶</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="191"/>
-        <location filename="mainwindow.ui" line="194"/>
-        <source>Open Folder</source>
-        <translation>打开文件夹</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="199"/>
+        <location filename="../mainwindow.ui" line="154"/>
         <source>Exit</source>
         <translation>退出</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="204"/>
-        <location filename="mainwindow.ui" line="207"/>
-        <source>Copy</source>
-        <translation>复制</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="212"/>
-        <location filename="mainwindow.ui" line="215"/>
-        <source>Paste</source>
-        <translation>粘贴</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="220"/>
-        <location filename="mainwindow.ui" line="223"/>
-        <source>Shear</source>
-        <translation>剪切</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="228"/>
-        <location filename="mainwindow.ui" line="231"/>
-        <source>Delete</source>
-        <translation>删除</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="236"/>
-        <location filename="mainwindow.ui" line="239"/>
-        <source>Edit</source>
-        <translation>编辑</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="244"/>
+        <location filename="../mainwindow.ui" line="159"/>
         <source>Save</source>
         <translation>保存</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="252"/>
-        <source>Save As...</source>
-        <translation>另存为...</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="255"/>
-        <source>Ctrl+Shift+S</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="260"/>
-        <source>English</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="265"/>
-        <source>中文</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="270"/>
-        <source>Check for updates</source>
-        <translation>检查更新</translation>
-    </message>
-    <message>
-        <source>Save(&amp;S)</source>
-        <translation type="vanished">保存(&amp;S)</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="247"/>
+        <location filename="../mainwindow.ui" line="162"/>
         <source>Ctrl+S</source>
-        <translation></translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>File read failure!</source>
-        <translation type="vanished">文件读取错误!</translation>
+        <location filename="../mainwindow.ui" line="167"/>
+        <source>Save As...</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="108"/>
-        <location filename="mainwindow.cpp" line="626"/>
-        <location filename="mainwindow.cpp" line="641"/>
-        <source>Error</source>
-        <translation>错误</translation>
+        <location filename="../mainwindow.ui" line="170"/>
+        <source>Ctrl+Shift+S</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="173"/>
-        <source>Cannot save the file %1:
- %2.</source>
-        <translation>无法保存文件 %1:\n %2.</translation>
+        <location filename="../mainwindow.ui" line="175"/>
+        <source>Check for updates</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="346"/>
-        <location filename="mainwindow.cpp" line="368"/>
-        <source>Warning</source>
-        <translation>警告</translation>
+        <location filename="../mainwindow.cpp" line="62"/>
+        <source>failed to register: &quot;Ctrl+Tab&quot;</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="347"/>
-        <source>Replace the button?</source>
-        <translation>确认替换该按钮?</translation>
+        <location filename="../mainwindow.cpp" line="80"/>
+        <source>Load Save Failure!</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="369"/>
-        <source>Delete the button?</source>
-        <translation>确认删除该按钮?</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.cpp" line="498"/>
+        <location filename="../mainwindow.cpp" line="274"/>
         <source>Save As</source>
-        <translation>另存为</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="599"/>
-        <location filename="mainwindow.cpp" line="613"/>
-        <source>Information</source>
-        <translation>提示</translation>
+        <location filename="../mainwindow.cpp" line="368"/>
+        <source>%1 is up tp date!</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="600"/>
-        <source>TyyAppManager is up tp date!</source>
-        <translation>TyyAppManager已是最新版本!</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.cpp" line="610"/>
+        <location filename="../mainwindow.cpp" line="371"/>
         <source>There is a new version!</source>
-        <translation>有一个新的版本!</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="611"/>
+        <location filename="../mainwindow.cpp" line="372"/>
         <source>Whether to download?</source>
-        <translation>是否前往下载!</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="642"/>
+        <location filename="../mainwindow.cpp" line="381"/>
         <source>Please check your network connection.</source>
-        <translation>请检查你的网络连接.</translation>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../dynamicdata.cpp" line="155"/>
+        <source>Can not save the file %1:
+ %2.</source>
+        <translation>无法保存文件 %1:
+ %2.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="19"/>
+        <source>%1 is runnig.</source>
+        <translation>%1 正在运行.</translation>
+    </message>
+    <message>
+        <location filename="../utils/shearplateutils.cpp" line="26"/>
+        <source>Replace the button?</source>
+        <translation>是否替换该按钮?</translation>
+    </message>
+    <message>
+        <location filename="../utils/shearplateutils.cpp" line="37"/>
+        <source>Delete the button?</source>
+        <translation>是否删除该按钮?</translation>
+    </message>
+</context>
+<context>
+    <name>TabWidget</name>
+    <message>
+        <location filename="../tabwidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../tabwidget.ui" line="18"/>
+        <source>Open Folder</source>
+        <translation>打开文件夹</translation>
+    </message>
+    <message>
+        <location filename="../tabwidget.ui" line="23"/>
+        <location filename="../tabwidget.ui" line="26"/>
+        <source>Delete</source>
+        <translation>删除</translation>
+    </message>
+    <message>
+        <location filename="../tabwidget.ui" line="31"/>
+        <location filename="../tabwidget.ui" line="34"/>
+        <source>Copy</source>
+        <translation>复制</translation>
+    </message>
+    <message>
+        <location filename="../tabwidget.ui" line="39"/>
+        <location filename="../tabwidget.ui" line="42"/>
+        <source>Edit</source>
+        <translation>编辑</translation>
+    </message>
+    <message>
+        <location filename="../tabwidget.ui" line="47"/>
+        <location filename="../tabwidget.ui" line="50"/>
+        <source>Shear</source>
+        <translation>剪切</translation>
+    </message>
+    <message>
+        <location filename="../tabwidget.ui" line="55"/>
+        <location filename="../tabwidget.ui" line="58"/>
+        <source>Paste</source>
+        <translation>粘贴</translation>
     </message>
 </context>
 </TS>
