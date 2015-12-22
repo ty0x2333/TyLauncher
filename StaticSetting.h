47a02f253cca7e919f9d7fa27e468297df33dfd3
luckytianyiyan <luckytianyiyan@gmail.com>
2015-12-22 23:17:03 +0800

    Release 2.0.0.0

--- StaticSetting.h
+++ StaticSetting.h
@@ -4,12 +4,12 @@
 #define APPLICATION_BIT             "x86"
 #define APP_URL                     "http://tyyappmanager.sinaapp.com"
 
-#define VER_FILEVERSION             0,1,1,10
-#define VER_FILEVERSION_STR         "0.1.1.10\0"
-#define VER_FILEVERSION_DISPLAY_STR "0.1.1.10"
+#define VER_FILEVERSION             2,0,0,0
+#define VER_FILEVERSION_STR         "2.0.0.0\0"
+#define VER_FILEVERSION_DISPLAY_STR "2.0.0.0"
 
-#define VER_PRODUCTVERSION          0,1,1,10
-#define VER_PRODUCTVERSION_STR      "0.1.1.10\0"
+#define VER_PRODUCTVERSION          2,0,0,0
+#define VER_PRODUCTVERSION_STR      "2.0.0.0\0"
 #define VER_PRODUCTNAME_STR         "TyLauncher"
 
 #define VER_COMPANYNAME_STR         "Tyy"
