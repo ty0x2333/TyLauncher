41c63b15fc3a0d7bb0213ee4abac0b2dc95ff182
luckytianyiyan@gmail.com <luckytianyiyan@gmail.com>
2016-01-02 22:18:28 +0800

    A  ReplaceAppButtonCommand

--- undo/commands.cpp
+++ undo/commands.cpp
@@ -1,5 +1,7 @@
 #include "commands.h"
-
+/**
+ * @brief RemoveAppButtonCommand
+ */
 RemoveAppButtonCommand::RemoveAppButtonCommand(AppButton *appBtn, AppInfo appInfo, QUndoCommand *parent)
     : QUndoCommand(parent)
 {
@@ -16,4 +18,24 @@ void RemoveAppButtonCommand::undo()
 void RemoveAppButtonCommand::redo()
 {
     _appBtn->clear();
+}
+/**
+ * @brief ReplaceAppButtonCommand
+ */
+ReplaceAppButtonCommand::ReplaceAppButtonCommand(AppButton *replacedAppBtn, AppInfo appInfo, AppInfo replacedAppInfo,QUndoCommand *parent)
+    : QUndoCommand(parent)
+{
+    setText(QObject::tr("Replace %1 to %2").arg(replacedAppInfo.name()).arg(appInfo.name()));
+    _replacedAppBtn = replacedAppBtn;
+    _appInfo = appInfo;
+    _replacedAppInfo = replacedAppInfo;
+}
+void ReplaceAppButtonCommand::undo()
+{
+    _replacedAppBtn->setDataFromAppInfo(_replacedAppInfo);
+}
+
+void ReplaceAppButtonCommand::redo()
+{
+    _replacedAppBtn->setDataFromAppInfo(_appInfo);
 }
--- undo/commands.h
+++ undo/commands.h
@@ -3,7 +3,9 @@
 
 #include <QUndoCommand>
 #include "widget/appbutton.h"
-
+/**
+ * @brief RemoveAppButtonCommand
+ */
 class RemoveAppButtonCommand : public QUndoCommand
 {
 public:
@@ -14,6 +16,22 @@ public:
 private:
     AppButton *_appBtn;
     AppInfo _appInfo;
+};
+
+/**
+ * @brief ReplaceAppButtonCommand
+ */
+class ReplaceAppButtonCommand : public QUndoCommand
+{
+public:
+    ReplaceAppButtonCommand(AppButton *replacedAppBtn, AppInfo appInfo, AppInfo replacedAppInfo,QUndoCommand *parent = 0);
+    void undo() Q_DECL_OVERRIDE;
+    void redo() Q_DECL_OVERRIDE;
+
+private:
+    AppButton *_replacedAppBtn;
+    AppInfo _appInfo;
+    AppInfo _replacedAppInfo;
 };
 
 #endif // COMMANDS_H
