445a8b5c19c2f2d18a6f38a0167705cff74c6731
luckytianyiyan@gmail.com <luckytianyiyan@gmail.com>
2016-01-02 21:19:07 +0800

    M AppButton add  AppInfo getter

--- widget/appbutton.cpp
+++ widget/appbutton.cpp
@@ -47,6 +47,11 @@ void AppButton::setDataFromAppInfo(const AppInfo &appInfo)
     setAppFileName(appInfo.fileName());
 }
 
+AppInfo AppButton::getAppInfo()
+{
+    return AppInfo(_appName, _fileName, this->text());
+}
+
 void AppButton::init()
 {
     setStyleSheet("text-align:left top;");
