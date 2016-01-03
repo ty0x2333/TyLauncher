3e9f4cab9c944d2df4cb5b940f41c0964b5737b3
luckytianyiyan@gmail.com <luckytianyiyan@gmail.com>
2016-01-03 16:22:00 +0800

    M  App StaticSetting

--- StaticSetting.h
+++ StaticSetting.h
@@ -1,21 +1,18 @@
 #ifndef STATICSETTING_H
 #define STATICSETTING_H
 
-#define APPLICATION_BIT             "x86"
-#define APP_URL                     "http://tyyappmanager.sinaapp.com"
+#define VER_FILEVERSION             2,0,1,0
+#define VER_FILEVERSION_STR         "2.0.1.0\0"
+#define VER_FILEVERSION_DISPLAY_STR "2.0.1.0"
 
-#define VER_FILEVERSION             2,0,0,0
-#define VER_FILEVERSION_STR         "2.0.0.0\0"
-#define VER_FILEVERSION_DISPLAY_STR "2.0.0.0"
-
-#define VER_PRODUCTVERSION          2,0,0,0
-#define VER_PRODUCTVERSION_STR      "2.0.0.0\0"
+#define VER_PRODUCTVERSION          2,0,1,0
+#define VER_PRODUCTVERSION_STR      "2.0.1.0\0"
 #define VER_PRODUCTNAME_STR         "TyLauncher"
 
-#define VER_COMPANYNAME_STR         "Tyy"
+#define VER_COMPANYNAME_STR         "luckytianyiyan"
 #define VER_FILEDESCRIPTION_STR     "文件快捷管理软件"
 #define VER_INTERNALNAME_STR        "tianyiyan.com"
-#define VER_LEGALCOPYRIGHT_STR      "Copyright (C) 2014-2015 Tyy All Rights Reserved"
+#define VER_LEGALCOPYRIGHT_STR      "Copyright (C) 2014-2016 luckytianyiyan All Rights Reserved"
 
 #define VER_ORIGINALFILENAME_STR    "TyLauncher"
 
