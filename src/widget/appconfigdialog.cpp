4342a8d446a5775ae3992a5a745b8a17d5b52a85
luckytianyiyan@gmail.com <luckytianyiyan@gmail.com>
2016-01-17 00:44:11 +0800

    A  Copyright

--- src/widget/appconfigdialog.cpp
+++ src/widget/appconfigdialog.cpp
@@ -1,4 +1,22 @@
-﻿#include "appconfigdialog.h"
+﻿/**
+ * Copyright (c) 2016, luckytianyiyan <luckytianyiyan@gmail.com>
+ * 
+ * This file is part of TyLauncher.
+ * 
+ * TyLauncher is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * TyLauncher is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with TyLauncher.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "appconfigdialog.h"
 #include "ui_appconfigdialog.h"
 #include "dynamicdata.h"
 #include "model/option.h"
